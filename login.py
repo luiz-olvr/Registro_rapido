0611838e9275a2f0921d19da6ed67ee792ad8a8a
Luiz Otávio da Rosa de Oliveira <luizotaviodarosadeoliveira@gmail.com>
2024-04-08 23:31:13 -0300

    Versão antiga

--- login.py
+++ login.py
@@ -1,25 +1,6 @@
 listaU = []
 listaS = []
-NovoU = " "
 val = 0
-
-def NovoUsu():
-    with open("listaUsu.txt", "r", encoding="utf-8") as usuarios:
-        users = usuarios.readlines()
-        print(users)
-    for linha in users:
-        if NovoU in linha:
-            return 1
-
-
-
-escolha = input("Deseja resetar/criar novos arquivos de Usuarios e senhas? [1] sim  ")
-if escolha == 1:
-    with open("listaUsu.txt", "w") as usuarios:
-        usuarios.write("Usuarios")
-    with open("listaSen.txt", "w") as senhas:
-        senhas.write("Senha")
-
 while True:
     if val == 1:
         print("login aprovado")
@@ -31,33 +12,16 @@ while True:
     print("*"*30)
     escolha  = int(input("Digite sua ação: "))
     if escolha == 0:
-        NovoU = (str(input("Digite o nome de usuario: ")).strip())
-        va = NovoUsu()
-        if va == 1:
-            print("\nUsuario já existente!!\n")
-        else:
-            senha = input("Digite sua senha: ")
-            with open("listaUsu.txt", "a")as username:
-                username.write(f"\n{NovoU}")
-            with open("listaSen.txt", "a") as SenhaN:
-                SenhaN.write(f"\n{senha}")
+        listaU.append(str(input("Digite o nome de usuario: ")).strip().capitalize())
+        listaS.append(str(input("Digite sua senha: ")).strip())
     if escolha == 1:
-        nome = str(input("Digite seu usuario: ")).strip()
+        nome = str(input("Digite seu usuario: ")).strip().capitalize()
         senha = str(input("Digite sua senha: ")).strip()
         for c in range(len(listaS)):
             if nome == listaU[c] and senha == listaS[c]:
                 val = 1
                 break
     if escolha == 2:
-        with open("listaUsu.txt", "r") as usuarios:
-             users = usuarios.readlines()
-        with open("listaSen.txt", "r") as sen:
-            senhas = sen.readlines()
-       
-        for c in range(len(users)):
-            print(f"\nUsuario: {users[c]} | Senha: {senhas[c]}\n")
-       # users usuarios.readlines()
-""" for c in range(len(listaS)):
-            print(f"\nUsuario: {listaU[c]}, Senha: {listaS[c]}")"""
-
+        for c in range(len(listaS)):
+            print(f"\nUsuario: {listaU[c]}, Senha: {listaS[c]}")
 
