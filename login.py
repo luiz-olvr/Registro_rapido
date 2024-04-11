4642dac59d8c7a2ec67bcd70092ee3b6ebcdf1c6
Luiz Otávio da Rosa de Oliveira <luizotaviodarosadeoliveira@gmail.com>
2024-04-10 23:44:50 -0300

    Update login.py
    Organização geral do código
    
    Loop de adição de usuários enquanto não for digitado 0
    
    Pergunta onde salvar os arquivos que serão criados/utilizados pelo programa

--- login.py
+++ login.py
@@ -1,63 +1,61 @@
-listaU = []
-listaS = []
-NovoU = " "
-val = 0
+from tkinter import filedialog
+from time import sleep
+from os import path
 
+NovoU = " "
+dirpath = filedialog.askdirectory()
+
+# Verifica se o novo usuario já existe no sistema se existir retorna 1
 def NovoUsu():
-    with open("listaUsu.txt", "r", encoding="utf-8") as usuarios:
+    with open(path.join (dirpath, "listaUsu.txt"), "r", encoding="utf-8") as usuarios:
         users = usuarios.readlines()
         print(users)
     for linha in users:
         if NovoU in linha:
             return 1
 
-
-
-escolha = input("Deseja resetar/criar novos arquivos de Usuarios e senhas? [1] sim  ")
-if escolha == 1:
-    with open("listaUsu.txt", "w") as usuarios:
+ # Criação dos arquivos base
+escolha = str(input("Deseja resetar/criar novos arquivos de Usuarios e senhas? [1] sim  "))
+if escolha == "1":
+    with open(path.join (dirpath, "listaUsu.txt"), "w") as usuarios:
         usuarios.write("Usuarios")
-    with open("listaSen.txt", "w") as senhas:
+    with open(path.join (dirpath, "listaSen.txt"), "w") as senhas:
         senhas.write("Senha")
 
 while True:
-    if val == 1:
-        print("login aprovado")
-        break
+
     print("*"*30)
     print("[ 0 ] Registrar novo usuario ")
-    print("[ 1 ] Logar")
-    print("[ 2 ] Mostrar usuarios")
+    print("[ 1 ] Mostrar usuarios")
+    print("[ 2 ] Finalizar programa")
     print("*"*30)
     escolha  = int(input("Digite sua ação: "))
-    if escolha == 0:
-        NovoU = (str(input("Digite o nome de usuario: ")).strip())
-        va = NovoUsu()
-        if va == 1:
-            print("\nUsuario já existente!!\n")
-        else:
-            senha = input("Digite sua senha: ")
-            with open("listaUsu.txt", "a")as username:
-                username.write(f"\n{NovoU}")
-            with open("listaSen.txt", "a") as SenhaN:
-                SenhaN.write(f"\n{senha}")
-    if escolha == 1:
-        nome = str(input("Digite seu usuario: ")).strip()
-        senha = str(input("Digite sua senha: ")).strip()
-        for c in range(len(listaS)):
-            if nome == listaU[c] and senha == listaS[c]:
-                val = 1
+    if escolha == 0: # Criação de um novo usuario
+        while True:
+            NovoU = (str(input("Digite o nome de usuario: ")).strip())
+            if NovoU == "0":
                 break
-    if escolha == 2:
-        with open("listaUsu.txt", "r") as usuarios:
+            va = NovoUsu()
+            if va == 1:
+                print("\nUsuario já existente!!\n")
+            else:
+                senha = input("Digite sua senha: ")
+                with open(path.join (dirpath, "listaUsu.txt"), "a")as username:
+                    username.write(f"\n{NovoU}")
+                with open(path.join (dirpath, "listaSen.txt"), "a") as SenhaN:
+                    SenhaN.write(f"\n{senha}")
+
+    if escolha == 1: # Ler todos os usuarios criados
+        with open(path.join (dirpath, "listaUsu.txt"), "r") as usuarios:
              users = usuarios.readlines()
-        with open("listaSen.txt", "r") as sen:
+        with open(path.join (dirpath, "listaSen.txt"), "r") as sen:
             senhas = sen.readlines()
        
-        for c in range(len(users)):
+        for c in range(len(users)): # Mostar todos os usuarios criados
             print(f"\nUsuario: {users[c]} | Senha: {senhas[c]}\n")
-       # users usuarios.readlines()
-""" for c in range(len(listaS)):
-            print(f"\nUsuario: {listaU[c]}, Senha: {listaS[c]}")"""
-
-
+        
+    if escolha == 2: # Finalizar o programa
+        print("\nFinalizando programa...\n")
+        sleep(1)
+        break
+                     
