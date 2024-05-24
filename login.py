0e0f56633963173b4eb875fcc18b5b078e5d21de
Luiz Otávio da Rosa de Oliveira <luizotaviodarosadeoliveira@gmail.com>
2024-05-24 00:51:46 -0300

    Updade  0.05
    Remoção das senhas e adição de e-mail no seu lugar!!

--- login.py
+++ login.py
@@ -7,38 +7,37 @@ import smtplib
 NovoU = " "
 indice = 0
 users = ' '
+Emails = ' '
 dirpath = filedialog.askdirectory()
-
 
 # Verifica se o novo usuario já existe no sistema se existir retorna 1
 def NovoUsu():
     with open(path.join (dirpath, "listaUsu.txt"), "r", encoding="utf-8") as usuarios:
         users = usuarios.readlines()
     for linha in users:
-
         if NovoU in linha:
             return 1
+    return 0
         
         
 # Verifica se o usuario existe no sistema e retorna a posição dele na lista
 def Veirficacao():
-    with open(path.join (dirpath, "listaUsu.txt"), "r", encoding="utf-8") as usuarios:
-        users = usuarios.readlines()
-        cuz = 0
-    for linha in users:
-        cuz += 1
-        if NovoU in linha:
-            return cuz -1
+    with open(path.join (dirpath, "listaEmail.txt"), "r", encoding="utf-8") as emai:
+       Emails = emai.readlines()
+    for linha in Emails:
+        if Gmail in linha:
+            return 1
+    return 0
 
-# Manda a senha do Usuario para o email dele
-def RecuperarSenha():
+# Mande um email para o usuario
+def MandarEmail():
     corpo_email = f"""
     <h1>Olá {users[indice]}</h1>
-    <p>Sua senha é {senhas[indice]}<p>
+    
     """
     
     msg = email.message.Message()
-    msg['Subject'] = f"Recuperação de senha {users[indice]}"
+    msg['Subject'] = f"Mensagem automatica, não responder {users[indice]}"
     msg['From'] = "Email aqui"
     msg['To'] = Gmail
     password = "Senha dos app aqui"
@@ -55,21 +54,20 @@ def RecuperarSenha():
 
 
  # Criação dos arquivos base
-escolha = str(input("Deseja resetar/criar novos arquivos de Usuarios e senhas? [1] sim  "))
+escolha = str(input("Deseja resetar/criar novos arquivos de Usuarios e Emails? [1] sim  "))
 if escolha == "1":
     with open(path.join (dirpath, "listaUsu.txt"), "w") as usuarios:
         usuarios.write("Usuarios")
-    with open(path.join (dirpath, "listaSen.txt"), "w") as senhas:
-        senhas.write("Senha")
+    with open(path.join (dirpath, "listaEmail.txt"), "w") as emai:
+        emai.write("Emails")
 
 while True:
 
     print("*"*30)
     print("[ 0 ] Registrar novo usuario ")
     print("[ 1 ] Mostrar usuarios")
-    print("[ 2 ] Finalizar programa")
-    print("[ 3 ] Fazer o login")
-    print("[ 4 ] Recuperar a senha")
+    print("[ 2 ] Mandar um email")
+    print("[ 3 ] Finalizar programa")
     print("*"*30)
     escolha  = str(input("Digite sua ação: "))
     if escolha == "0": # Criação de um novo usuario
@@ -86,65 +84,41 @@ while True:
                 indice = Veirficacao() 
                 print(indice)
             else:
-                senha = input("Digite sua senha: ").strip()
+                email = input("Digite seu email: ").strip()
                 with open(path.join (dirpath, "listaUsu.txt"), "a")as username:
                     username.write(f"\n{NovoU}")
-                with open(path.join (dirpath, "listaSen.txt"), "a") as SenhaN:
-                    SenhaN.write(f"\n{senha}")
+                with open(path.join (dirpath, "listaEmail.txt"), "a") as emails:
+                    emails.write(f"\n{email}")
                     sleep(0.5)
                     print("\nUsuario salvo")
 
     elif escolha == "1": # Ler todos os usuarios criados
         with open(path.join (dirpath, "listaUsu.txt"), "r") as usuarios:
              users = usuarios.readlines()
-        with open(path.join (dirpath, "listaSen.txt"), "r") as sen:
-            senhas = sen.readlines()
+        with open(path.join (dirpath, "listaEmail.txt"), "r") as emai:
+            emai = emai.readlines()
         sleep(0.2)
         print("Aguarde...")
         sleep(1.5)
         for c in range(len(users)): # Mostar todos os usuarios criados
-            print(f"\nUsuario: {users[c]} | Senha: {senhas[c]}\n")
+            print(f"\nUsuario: {users[c]} | Email: {emai[c]}\n")
         
-    elif escolha == "2": # Finalizar o programa
+  
+                        
+    elif escolha == "2":   # Mandar um email
+        Gmail = (str(input(f"Digite seu email: ")).strip())
+        va = Veirficacao()
+        if va == 1:
+            MandarEmail()
+        else:
+            print("Email não encontrado")
+
+                    
+    elif escolha == "3": # Finalizar o programa
         sleep(0.2)
         print("\nFinalizando programa...\n")
         sleep(1)
-        break
-    
-    elif escolha == "3": # Login
-        while True:
-            with open(path.join (dirpath, "listaUsu.txt"), "r") as usuarios:
-                users = usuarios.readlines()
-            with open(path.join (dirpath, "listaSen.txt"), "r") as sen:
-                senhas = sen.readlines()
-            while True:
-                NovoU = (str(input("Digite o nome de usuario: ")).strip())
-                indice = Veirficacao() 
-                if indice == 0:
-                    print("\nUsuario Não existente!!\n")
-                else:
-                    SenhaConfir = (str(input(f"\nDigite sua senha: ")).strip())
-                    if NovoU in users[indice] and SenhaConfir in senhas[indice]:
-                        print("Login feito")
-                    else:
-                        print("Senha incorreta!!\n")
-                        
-    elif escolha == "4":   # Recuperar a senha
-        
-        with open(path.join (dirpath, "listaUsu.txt"), "r") as usuarios:
-                users = usuarios.readlines()
-        with open(path.join (dirpath, "listaSen.txt"), "r") as sen:
-                senhas = sen.readlines()
-        while True:              
-                NovoU = (str(input(f"Digite o nome de usuario: ")).strip())
-                indice = Veirficacao() 
-                if indice == 0:
-                    print("\nUsuario Não existente!!\n")
-                else:
-                    Gmail = (str(input(f"Digite seu email: ")).strip())
-                    RecuperarSenha()
-                    break
-                    
+        break 
     
     else:
         sleep(0.2)
