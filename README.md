2158da8417fc3364690f78947c6cc1bbf6b4bb43
Luiz-olvr <luizotaviodarosadeoliveira@gmail.com>
2024-09-16 18:42:41 -0300

    pequenas alterações

--- README.md
+++ README.md
@@ -7,9 +7,11 @@ O projeto consiste em registrar usuarios e seus E-mails e salvar em um arquivo d
 
 ## Onde pode ser usado??
 
-Pode ser usado em:
-* Eventos escolares
-* Sorteios
+Aplicações: Esse sistema pode ser utilizado em cenários onde se precisa gerenciar uma lista de contatos de forma simples e sem a complexidade de um banco de dados, como:
+
+  *  Eventos de pequeno porte.
+  *  Grupos de trabalho ou estudo.
+  *  Projetos pessoais ou profissionais que exigem o gerenciamento de uma pequena base de dados local.
 
 Caso ache algum erro contate:
   <a href = "mailto:luizrolvr@gmail.com"><img src="https://img.shields.io/badge/-Gmail-%23333?style=for-the-badge&logo=gmail&logoColor=white" target="_blank"></a>
--- Registro_rapido.py
+++ Registro_rapido.py
@@ -1,12 +1,14 @@
 from os import path
 from tkinter import filedialog
 dirpath = filedialog.askdirectory()
+lista = list()
 
 criar = int(input("Deseja criar novos arquivos[ 0 ]?\nContinuar com os antigos[ 1 ]?\nEscolha: "))
 if criar == 1:
-    pass 
-
-lista = list()
+        with open(path.join(dirpath, "Arquivo.txt"), "r") as leitura:
+            for linha in leitura:
+                nome, email = linha.strip().split(">") 
+                lista.append({"nome": nome, "email": email})  
 
 while True:
     print("""
@@ -17,10 +19,13 @@ while True:
           [ 4 ] gerar arquivo de texto
           [ 5 ] encerrar programa
          """)
+    
     esc = int(input("Digite a opção: "))
+    
     if esc == 0:
         nome = str(input("Digite o nome do usuario: "))
         email = str(input("Digite o email: "))
+        
         for c in range(0,len(lista)):
             if  nome in lista[c]['nome'] and email in lista[c]['email']:
                 print("Usuario já existente!")
@@ -48,18 +53,10 @@ while True:
         
     elif esc == 4:
         with open(path.join(dirpath, "Arquivo.txt"), "w", encoding="utf-8") as arquivo:
-           # arquivo.write(f'{"Nomes":<40} {"Emails":>7}\n\n')
             for c in range(0,len(lista)):
                 arquivo.write(f'{lista[c]['nome']: <40} > {lista[c]['email']:>7}\n')
-                #arquivo.write(f'{lista[c]}\n')
-            
+                
     elif esc == 5:
-        with open(path.join(dirpath, "Arquivo.txt"), "r") as leitura:
-            for linha in leitura:
-                nome, email = linha.strip().split(">") 
-                lista.append({"nome": nome, "email": email})  
-
-    elif esc == 6:
         print("Encerrando programa!")
         break
 
